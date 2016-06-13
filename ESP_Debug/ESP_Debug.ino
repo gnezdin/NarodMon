1d98e5cf7930a9ad3ef3b777d5a5b964799f3f0d
sergewddm <serge_wddm@tut.by>
2016-06-13 15:41:21 +0300

    last

--- ESP_Debug/ESP_Debug.ino
+++ ESP_Debug/ESP_Debug.ino
@@ -14,9 +14,12 @@ const char* ssid     = "WdLink"; // название и пароль точки 
 const char* password = "aeroglass";
 
 String tsApiKey = "6EE0PANPMO4FELI6"; 
-const char* tsServer = "184.106.153.149";
+const char* tsServer = "api.thingspeak.com";
 
-int send_counter_timeout = 60;
+// период отправки данных, *100 мс
+#define SEND_TIMEOUT 6000
+
+int send_counter_timeout = SEND_TIMEOUT;
 long send_counter = 0;
 
 void SetLedError(bool val)
@@ -24,54 +27,8 @@ void SetLedError(bool val)
   digitalWrite(LED_RED, val);
 }
 
-
-void setup() 
+void SendDataGet()
 {
-  pinMode(ADC, INPUT);
-  
-  pinMode(LED, OUTPUT);
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_BLUE, OUTPUT);
-  digitalWrite(LED, 1);
-  digitalWrite(LED_RED, 0);
-  digitalWrite(LED_GREEN, 0);
-  digitalWrite(LED_BLUE, 0);
-
-  Serial.begin(115200);
-  delay(100);
-
-  // подключаемся к WiFi сети
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  
-  digitalWrite(LED, 0);
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void loop() 
-{
-  int lightSensorValue = -1;
-  
-  send_counter_timeout ++;
-  digitalWrite(LED_GREEN, 0);
-
-  if (send_counter_timeout >= 60)
-  {
     SetLedError(0);
     digitalWrite(LED_GREEN, 1);
     Serial.println("");
@@ -89,7 +46,7 @@ void loop()
     }
     Serial.println("[SEND_COUNTER] WiFi connected");
 
-    lightSensorValue = analogRead(ADC);
+    int lightSensorValue = analogRead(ADC);
     
     // Use WiFiClient class to create TCP connections
     WiFiClient client;
@@ -129,13 +86,141 @@ void loop()
       Serial.print(line);
     }
     
+    client.stop();
+    
     Serial.println();
     Serial.println("<-[SEND_COUNTER] closing connection");
     Serial.println();
     digitalWrite(LED_GREEN, 0);
+}
+
+void SendDataPost()
+{
+    SetLedError(0);
+    digitalWrite(LED_GREEN, 1);
+    Serial.println("");
+    Serial.println("->[SEND_COUNTER]");
+ 
+    send_counter_timeout = 0;
+    send_counter ++;
+    
+    if (WiFi.status() != WL_CONNECTED)
+    {
+      Serial.println("[SEND_COUNTER] WiFi not connected!");
+      SetLedError(1);
+      digitalWrite(LED_GREEN, 0);
+      return;
+    }
+    Serial.println("[SEND_COUNTER] WiFi connected");
+
+    int lightSensorValue = analogRead(ADC);
+    
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(tsServer, httpPort)) 
+    {
+      SetLedError(1);
+      digitalWrite(LED_GREEN, 0);
+      Serial.println("[SEND_COUNTER] Connection to Host Failed!");
+      return;
+    }
+    Serial.println("[SEND_COUNTER] Connected to Host");
+    // Создаем POST для запроса
+    String postStr = "api_key=" + tsApiKey;
+    postStr +="&field1=";
+    postStr += String(lightSensorValue);
+    postStr +="&field2=";
+    postStr += String(send_counter);
+    postStr += "\r\n\r\n";
+
+    Serial.println("[SEND_COUNTER] Requesting URL:");
+    Serial.println(postStr);
+
+    // отправляем запрос на сервер
+    client.print("POST /update HTTP/1.1\n"); 
+    client.print("Host: api.thingspeak.com\n"); 
+    client.print("Connection: close\n"); 
+    client.print("X-THINGSPEAKAPIKEY: "+tsApiKey+"\n"); 
+    client.print("Content-Type: application/x-www-form-urlencoded\n"); 
+    client.print("Content-Length: "); 
+    client.print(postStr.length()); 
+    client.print("\n\n"); 
+    client.print(postStr);
+     
+    client.flush(); // ждем отправки всех данных
+    Serial.println("[SEND_COUNTER] Request sended");
+
+    Serial.println("[SEND_COUNTER] Host Answer:");
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available())
+    {
+      String line = client.readStringUntil('\r');
+      //char line = client.read();
+      Serial.print(line);
+    }
+    
+    client.stop();
+    
+    Serial.println();
+    Serial.println("<-[SEND_COUNTER] closing connection");
+    Serial.println();
+    digitalWrite(LED_GREEN, 0);
+}
+
+
+void setup() 
+{
+  pinMode(ADC, INPUT);
+  
+  pinMode(LED, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  digitalWrite(LED, 1);
+  digitalWrite(LED_RED, 1);
+  digitalWrite(LED_GREEN, 1);
+  digitalWrite(LED_BLUE, 0);
+
+  Serial.begin(115200);
+  delay(10000);
+  digitalWrite(LED_RED, 0);
+  digitalWrite(LED_GREEN, 0);
+  delay(1000);
+
+  // подключаемся к WiFi сети
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
   }
   
-  delay(1000);
+  digitalWrite(LED, 0);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() 
+{  
+  send_counter_timeout ++;
+  digitalWrite(LED_GREEN, 0);
+
+  if (send_counter_timeout >= SEND_TIMEOUT)
+  {
+    SendDataPost();
+  }
+  
+  delay(100);
 }
 
 
